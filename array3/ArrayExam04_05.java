2ac515a80bcd58839440cc4b4f16e44f3e820348
Wonjung <gimpo5975@naver.com>
2024-01-16 20:08:44 +0900

    arrayExam04_05 메소드로 main 간결화

--- array3/ArrayExam04_05.java
+++ array3/ArrayExam04_05.java
@@ -16,11 +16,12 @@ public class ArrayExam04_05 {
 	// while문 제어용 변수 초기화
 	static boolean run = true;
 	
+	//entry point
 	public static void main(String[] args) {
 		// 배열 초기화
 		String[] beverage = new String[4];
-		int quantity1[] = new int[4];
-		int price1[] = new int[4];
+		int arrayQuantity[] = new int[4];
+		int arrayPrice[] = new int[4];
 		
 		while (run) {
 			System.out.printf("음료 번호를 선택하세요 \n1.아아(2500) | 2.라떼(4500) | 3.콜라(1500) | 4.홍차(5500) | 0.주문 취소 >>");// 안내문구
@@ -30,9 +31,9 @@ public class ArrayExam04_05 {
 			case 1:// 1번 아아
 				System.out.printf("주문 수량을 입력 >> ");
 				quantity = Integer.parseInt(sc.nextLine());// 주문수량 받기
-				quantity1[0] = quantity;// 아메리카노 주문수량 배열에 저장
+				arrayQuantity[0] = quantity;// 아메리카노 주문수량 배열에 저장
 				price = quantity * COFFE_PRICE;// 금액 연산
-				price1[0] = price;// 금액배열에 금액 저장
+				arrayPrice[0] = price;// 금액배열에 금액 저장
 				beverage[0] = "아아";// 음료배열에 아아 저장
 				System.out.printf("주문을 계속하시겠습니까? \nY:주문계속 | N:주문취소");// 주문여부 물어보기
 				order = sc.nextLine();// 주문여부 입력
@@ -47,9 +48,9 @@ public class ArrayExam04_05 {
 			case 2:// 2번 라떼
 				System.out.printf("주문 수량을 입력 >> ");
 				quantity = Integer.parseInt(sc.nextLine());// 주문수량 받기
-				quantity1[1] = quantity;// 라떼 주문수량 배열에 저장
+				arrayQuantity[1] = quantity;// 라떼 주문수량 배열에 저장
 				price = quantity * LATTE_PRICE;// 금액 연산
-				price1[1] = price;// 금액배열에 금액 저장
+				arrayPrice[1] = price;// 금액배열에 금액 저장
 				beverage[1] = "라떼";// 음료배열에 라떼 저장
 				System.out.printf("주문을 계속하시겠습니까? \nY:주문계속 | N:주문취소");// 주문여부 물어보기
 				order = sc.nextLine();// 주문여부 입력
@@ -64,9 +65,9 @@ public class ArrayExam04_05 {
 			case 3:// 3번 콜라
 				System.out.printf("주문 수량을 입력 >> ");
 				quantity = Integer.parseInt(sc.nextLine());// 주문수량 받기
-				quantity1[2] = quantity;// 콜라 주문수량 배열에 저장
+				arrayQuantity[2] = quantity;// 콜라 주문수량 배열에 저장
 				price = quantity * COKE_PRICE;// 금액 연산
-				price1[2] = price;// 금액배열에 금액 저장
+				arrayPrice[2] = price;// 금액배열에 금액 저장
 				beverage[2] = "콜라";// 음료배열에 콜라 저장
 				System.out.printf("주문을 계속하시겠습니까? \nY:주문계속 | N:주문취소");// 주문여부 물어보기
 				order = sc.nextLine();// 주문여부 입력
@@ -81,9 +82,9 @@ public class ArrayExam04_05 {
 			case 4:// 4번 홍차
 				System.out.printf("주문 수량을 입력 >> ");
 				quantity = Integer.parseInt(sc.nextLine());// 주문수량 받기
-				quantity1[3] = quantity;// 홍차 주문수량 배열에 저장
+				arrayQuantity[3] = quantity;// 홍차 주문수량 배열에 저장
 				price = quantity * RED_TEA;// 금액 연산
-				price1[3] = price;// 금액배열에 금액 저장
+				arrayPrice[3] = price;// 금액배열에 금액 저장
 				beverage[3] = "홍차";// 음료배열에 홍차 저장
 				System.out.printf("주문을 계속하시겠습니까? \nY:주문계속 | N:주문취소");// 주문여부 물어보기
 				order = sc.nextLine();// 주문여부 입력
@@ -103,16 +104,16 @@ public class ArrayExam04_05 {
 		} // end of while
 			// 출력
 		System.out.println("****************************************************");
-		for (int i = 0; i < price1.length; i++) {
-			if (beverage[i] == null || quantity1[i] == 0 || price1[i] == 0) { // 주문안한 음료는 출력에서 제외
+		for (int i = 0; i < arrayPrice.length; i++) {
+			if (beverage[i] == null || arrayQuantity[i] == 0 || arrayPrice[i] == 0) { // 주문안한 음료는 출력에서 제외
 				continue;
 			} // end of if
-			System.out.printf("%s\t%d잔 주문\t주문금액\t%,3d원\n", beverage[i], quantity1[i], price1[i]);
+			System.out.printf("%s\t%d잔 주문\t주문금액\t%,3d원\n", beverage[i], arrayQuantity[i], arrayPrice[i]);
 		} // end of for
 		System.out.println("****************************************************");
 
-		for (int i = 0; i < price1.length; i++) {// 금액 총합계산
-			sum += price1[i];
+		for (int i = 0; i < arrayPrice.length; i++) {// 금액 총합계산
+			sum += arrayPrice[i];
 		} // end of for
 
 		// 총 금액 출력
